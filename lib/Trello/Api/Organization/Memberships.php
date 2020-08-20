7b64811145be5dbd433d815b3f52075054081971
Christian Labonté <cbonte99@gmail.com>
2020-08-20 11:09:49 -0400

    Added support for organizations membership API

---
+++ lib/Trello/Api/Organization/Memberships.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Trello\Api\Organization;
+
+
+class Memberships extends \Trello\Api\AbstractApi
+{
+    /**
+     * Base path of board power ups api
+     * @var string
+     */
+    protected $path = 'organizations/#id#/memberships';
+
+}
